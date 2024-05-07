d76a3b6067b2cc33bed243549eac08c882630ea1
Yury Sychyk <kauz@tut.by>
2024-05-07 21:24:09 +0300

    outlet buttons to variables and metods

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var currentNumber: UILabel!
+    
+   
+    @IBAction func button0(_ sender: UIButton) {
+        currentNumber.text = "0"
+    }
+    
+    @IBAction func button1(_ sender: UIButton) {
+        currentNumber.text = "1"
+    }
+    
+    @IBAction func button3(_ sender: UIButton) {
+        currentNumber.text = "3"
+    }
+    
 }
+
 
